817f229d3fe6496b1ef6f79be51b171c8cb70aaf
ripomwp <you@example.com>
2025-05-18 13:16:59 +0400

    new file add

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Code Start</title>
+</head>
+<body>
+    <?php
 
-echo "Hello Bangladesh";
-
+    echo (" Php Code Start");
+    ?>
+</body>
+</html>
